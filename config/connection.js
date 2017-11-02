90fc5cdc61cf3a35f5ff1354f51a23bc3068bef2
Adrian Sirko <cipko@Adrians-MacBook-Air.local>
2017-11-02 18:22:12 -0400

    Burger

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,17 @@
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}
+else{
+    connection=mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'hacktheplanet',
+        database:'todoagain_db'
+    });
+};
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
