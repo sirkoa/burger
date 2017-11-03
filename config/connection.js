6859db0b7bc534c2b45d1ef2ad1d4e02b73bf35f
Adrian Sirko <cipko@Adrians-MacBook-Air.local>
2017-11-02 20:51:55 -0400

    updated sql

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,17 @@
 var mysql = require("mysql");
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-}
-else{
-    connection=mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'hacktheplanet',
-        database:'todoagain_db'
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db',
+        port: 8889
     });
 };
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers",
-    port: 8889
-});
 
 connection.connect(function(err) {
     if (err) {
